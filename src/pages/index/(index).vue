7f43ea4ecf2d5f3cbd65f0562e53ec5cd85d0b20
Yuumi-Na <yumiii.nakano@gmail.com>
2026-08-21 13:51:14 +0700

    v.5-Accept with Chinese language-372

--- src/pages/index/(index).vue
+++ src/pages/index/(index).vue
@@ -3,7 +3,7 @@
     <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md" style="max-width: 600px">
       <q-input v-model="name" filled label="Your name *" hint="Name and surname" />
       <q-input v-model="age" filled type="number" label="Your age *" />
-      <q-toggle v-model="accept" label="I accept the license and terms" />
+      <q-toggle v-model="accept" label="I accept the license and terms (我接受许可和条款语言) " />
       <div>
         <q-btn label="SUBMIT" type="submit" color="primary" />
         <q-btn label="RESET" type="reset" color="primary" flat class="q-ml-sm" />
